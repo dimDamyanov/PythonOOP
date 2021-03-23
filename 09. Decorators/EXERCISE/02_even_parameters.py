64d377637b804f48a89e3d83166ea71623be7a03
Dimitar I. Damyanov <dim.damianov@gmail.com>
2021-03-23 17:44:56 +0200

    Added solution to Decorators - Exercise

---
+++ 09. Decorators/EXERCISE/02_even_parameters.py
@@ -0,0 +1,33 @@
+import functools
+
+
+def even_parameters(func):
+    @functools.wraps(func)
+    def wrapper(*args):
+        if all([isinstance(param, int) and param % 2 == 0 for param in args]):
+            return func(*args)
+        else:
+            return 'Please use only even numbers!'
+
+    return wrapper
+
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
+
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result
+
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))
---
+++ 09. Decorators/EXERCISE/03_bold_italic_underline.py
@@ -0,0 +1,44 @@
+import functools
+
+
+def make_bold(func):
+    @functools.wraps(func)
+    def wrapper(*args):
+        return '<b>' + func(*args) + '</b>'
+
+    return wrapper
+
+
+def make_italic(func):
+    @functools.wraps(func)
+    def wrapper(*args):
+        return '<i>' + func(*args) + '</i>'
+
+    return wrapper
+
+
+def make_underline(func):
+    def wrapper(*args):
+        return '<u>' + func(*args) + '</u>'
+
+    return wrapper
+
+
+@make_bold
+@make_italic
+@make_underline
+def greet(name):
+    return f"Hello, {name}"
+
+
+print(greet("Peter"))
+
+
+@make_bold
+@make_italic
+@make_underline
+def greet_all(*args):
+    return f"Hello, {', '.join(args)}"
+
+
+print(greet_all("Peter", "George"))
